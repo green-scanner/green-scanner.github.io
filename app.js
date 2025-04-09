386369a33d95a7c1087397a8915ea42826edee42
green-scanner <andreas.linne@hotmail.com>
2025-04-09 12:53:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ async function fetchProductInfo(barcode) {
         if (loading) loading.hidden = false;
         console.log('Fetching product info for barcode:', barcode);
 
-        const response = await fetch('http://127.0.0.1:8080/scan', {
+        const response = await fetch('http://green-scanner.github.io//scan', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ barcode })
